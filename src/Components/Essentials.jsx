b10140fc3837375d11a79aa875ef3fd2d92a57ee
Emeka Victor <emekav233@gmail.com>
2024-11-01 17:32:41 +0100

    chnages

--- src/Components/Essentials.jsx
+++ src/Components/Essentials.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Essentials = () => {
   return (
     <div>
-      <img src="images/three_cheerful_young_women_embracing" alt="" />
+      <img src="images/three_cheerful_young_women_embracing.jpg" alt="" />
     </div>
   );
 };
