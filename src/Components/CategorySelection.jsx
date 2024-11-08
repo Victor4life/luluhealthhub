e0de877d3d85e897a7b690bb302321646da99866
Emeka Victor <emekav233@gmail.com>
2024-11-08 07:38:15 +0100

    chnaegs

--- src/Components/CategorySelection.jsx
+++ src/Components/CategorySelection.jsx
@@ -1,37 +1,26 @@
 import React from "react";
 
-const CategorySelection = ({ onSelectCategory, activeCategory }) => {
-  const categories = [
-    "Reproductive Health",
-    "Mental Health",
-    "Hormonal Health",
-    "Sexual Health",
-    "Lifestyle & Wellness",
-    "Nutritional Health",
-  ];
-
+const CategorySelection = ({ onSelectTag, activeTag, tags }) => {
   return (
     <div className="px-4 mb-8 py-1 text-gray-900">
       <div className="overflow-x-auto flex items-center space-x-2 pb-2">
         <button
-          onClick={() => onSelectCategory(null)}
+          onClick={() => onSelectTag(null)}
           className={`whitespace-nowrap px-4 py-2 rounded-full ${
-            activeCategory ? "bg-gray-200" : "bg-blue-500 text-white"
+            activeTag ? "bg-gray-200" : "bg-blue-500 text-white"
           }`}
         >
           All
         </button>
-        {categories.map((category) => (
+        {tags.map((tag) => (
           <button
-            onClick={() => onSelectCategory(category)}
+            onClick={() => onSelectTag(tag)}
             className={`whitespace-nowrap px-4 py-2 rounded-full ${
-              activeCategory === category
-                ? "bg-blue-500 text-white"
-                : "bg-gray-200"
+              activeTag === tag ? "bg-blue-500 text-white" : "bg-gray-200"
             }`}
-            key={category}
+            key={tag}
           >
-            {category}
+            {tag}
           </button>
         ))}
       </div>
